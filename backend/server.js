dfec529c712aca9a6ead0438a270daa715be96be
Komatigunta Sai Sarani <sarani9491@gmail.com>
2026-05-23 14:38:51 +0530

    Update server.js to include todo routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const todoRoutes = require('./routes/todos');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -19,6 +21,9 @@ app.get('/', (req, res) => {
 app.get('/api/health', (req, res) => {
   res.json({ status: 'Server is running' });
 });
+
+// Todo routes
+app.use('/api/todos', todoRoutes);
 
 // Start server
 app.listen(PORT, () => {
